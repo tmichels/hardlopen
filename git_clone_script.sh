aeb29198d5ce8e1c76e2af10a80bd7b7dce08116
Thomas Michels <thomas_michels@outlook.com>
2021-11-03 10:59:27 +0100

    Remove outdated installation script, add clone script

---
+++ git_clone_script.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+
+git clone git@github.com:tmichels/backend-runs.git
+git clone git@github.com:tmichels/backend-tcxreader.git
+git clone git@github.com:tmichels/backend-strava.git
+git clone git@github.com:tmichels/frontend.git
+git clone git@github.com:tmichels/backend-location.git
