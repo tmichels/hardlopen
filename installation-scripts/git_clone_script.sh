75bb16c42b86775761f09be0d016e68b68bdc785
Thomas Michels <thomas_michels@outlook.com>
2021-11-17 09:36:43 +0100

    Add file to store Github ghp

--- installation-scripts/git_clone_script.sh
+++ installation-scripts/git_clone_script.sh
@@ -20,6 +20,7 @@ echo $open_cage_key > backend-location/src/main/resources/api-key/OpenCageAPIKey
 read -p 'Enter the Strava secret: ' strava_secret
 echo $strava_secret > backend-strava/src/main/resources/api-key/StravaSecret.txt
 echo
+$access_token > github-ghp.txt
 
 echo The repositories were successfully cloned. The structure is expected to be """
 ├── hardlopen
@@ -29,6 +30,7 @@ echo The repositories were successfully cloned. The structure is expected to be 
 │   ├── backend-tcxreader
 │   ├── docker-compose.yml
 │   ├── frontend
+│   ├── github-ghp.txt
 │   ├── installation-scripts
 │   ├── README.md
 │   └── sample_runs
