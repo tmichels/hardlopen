a9f5d4a94d38e1670e237ec4e56badabcee6bc0c
Thomas Michels <thomas_michels@outlook.com>
2022-01-12 13:09:23 +0100

    Correction to pull script

--- installation-scripts/git_pull_all.sh
+++ installation-scripts/git_pull_all.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/bash
 
-git pull ../backend-runs
-git pull ../backend-tcxreader
-git pull ../backend-strava
-git pull ../frontend
-git pull ../backend-location
-git pull ../backend-trace-maker
+git pull ./backend-runs
+git pull ./backend-tcxreader
+git pull ./backend-strava
+git pull ./frontend
+git pull ./backend-location
+git pull ./backend-trace-maker
