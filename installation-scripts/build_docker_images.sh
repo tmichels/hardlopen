4c9d91f04c87ab5cca9b79d9cb89b893a3e9f648
Thomas Michels <thomas_michels@outlook.com>
2022-01-12 13:09:50 +0100

    Make build script execution parallel

--- installation-scripts/build_docker_images.sh
+++ installation-scripts/build_docker_images.sh
@@ -1,8 +1,8 @@
 !# /usr/bin/bash
 
-docker build -t backend-runs ./backend-runs
-docker build -t backend-tcxreader ./backend-tcxreader
-docker build -t backend-strava ./backend-strava 
-docker build -t backend-location ./backend-location
-docker build -t backend-trace-maker ./backend-trace-maker
+docker build -t backend-runs ./backend-runs &
+docker build -t backend-tcxreader ./backend-tcxreader &
+docker build -t backend-strava ./backend-strava &
+docker build -t backend-location ./backend-location &
+docker build -t backend-trace-maker ./backend-trace-maker &
 docker build -t frontend ./frontend
