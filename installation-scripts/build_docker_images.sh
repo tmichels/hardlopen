1b6e493a6bdd208c09e02f0dd87d6b552324dc32
Thomas Michels <thomas_michels@outlook.com>
2022-03-30 13:36:20 +0200

    Avoid not exiting after build

--- installation-scripts/build_docker_images.sh
+++ installation-scripts/build_docker_images.sh
@@ -6,3 +6,5 @@ docker build -t backend-strava ./backend-strava &
 docker build -t backend-location ./backend-location &
 docker build -t backend-trace-maker ./backend-trace-maker &
 docker build -t frontend ./frontend
+
+exit 0
