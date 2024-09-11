78e56c37c09e4916042a8331b1d734ee7fc07cdf
Thomas Michels <thomas_michels@outlook.com>
2024-09-11 22:12:45 +0200

    Replace "docker-compose" with "docker compose" for button

--- installation-scripts/button/button-action.sh
+++ installation-scripts/button/button-action.sh
@@ -9,11 +9,12 @@ open_browser () {
 docker network prune -f
 docker ps | grep -q backend-runs
 app_is_up=$?
+echo App status $app_is_up
 
 if [ $app_is_up -eq 1 ]; then
 	sh /home/thomas/projecten/hardlopen/installation-scripts/build_docker_images.sh
 	open_browser &
-	docker-compose -f /home/thomas/projecten/hardlopen/docker-compose.yml up --force-recreate
+	docker compose -f /home/thomas/projecten/hardlopen/docker-compose.yml up --force-recreate
 else
-	docker-compose -f /home/thomas/projecten/hardlopen/docker-compose.yml down
+	docker compose -f /home/thomas/projecten/hardlopen/docker-compose.yml down
 fi
