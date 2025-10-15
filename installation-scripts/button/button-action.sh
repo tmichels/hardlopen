37861db214fb1ffc8083a7b60d24e7df35bbbc00
Thomas Michels <thomas_michels@outlook.com>
2025-10-15 09:12:55 +0200

    Voeg vaadin toe aan gitignore

--- installation-scripts/button/button-action.sh
+++ installation-scripts/button/button-action.sh
@@ -9,12 +9,13 @@ open_browser () {
 podman network prune -f
 podman ps | grep -q backend-runs
 app_is_up=$?
-echo App status $app_is_up
+echo App status: $app_is_up
+cd /home/thomas/projecten/hardlopen
 
 if [ $app_is_up -eq 1 ]; then
-	sh /home/thomas/projecten/hardlopen/installation-scripts/build_podman_images.sh
+	bash installation-scripts/build_podman_images.sh
 	open_browser &
-	podman compose -f /home/thomas/projecten/hardlopen/podman-compose.yml up --force-recreate
+	podman compose up
 else
-	podman compose -f /home/thomas/projecten/hardlopen/podman-compose.yml down
+	podman compose down
 fi
