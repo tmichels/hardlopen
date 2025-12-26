c113359db2d4d9de7cb883f1e0b432d54e940baa
Thomas Michels <thomas_michels@outlook.com>
2025-12-26 13:12:49 +0100

    Aanpassing arc script

--- installation-scripts/archiveer.sh
+++ installation-scripts/archiveer.sh
@@ -4,5 +4,5 @@ find /home/thomas/TomTomWatch/working \( -iname "*.ttbin" -o -name "*.gpx" -o -i
 
 echo
 
-read -p "Enter om bovenstaande bestanden te verplaatsen"
+read -p "Enter om bovenstaande bestanden te verplaatsen naar /home/thomas/Dropbox/bestanden/Hardlopen/Hardloopdata/"
 find /home/thomas/TomTomWatch/working \( -iname "*.ttbin" -o -name "*.gpx" -o -iname "*tcx" \) -type f -exec mv {} /home/thomas/Dropbox/bestanden/Hardlopen/Hardloopdata/ \;
